a19818476e9dc89a67694a3b2fcb6c4a10a02827
Tom Straszewski <tomasz@cambridge.ventures>
2020-04-04 21:11:27 +0100

    adds sound to orb

--- Orb.gd
+++ Orb.gd
@@ -4,6 +4,8 @@ export(String, "blue", "red", "green") var orb_color = "blue"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var delay_counter = 0
+var picked = false
 
 var orb_xy = { "blue" : Vector2(0,480), "green" : Vector2(0,432), "red" : Vector2(192, 432)}
 # Called when the node enters the scene tree for the first time.
@@ -16,8 +18,19 @@ func _ready():
 		$Sprite.region_rect = Rect2(orb_xy["red"],Vector2(48,48))
 	pass # Replace with function body.
 
-
+func _physics_process(delta):
+		if picked:
+			delay_counter += 1
+		if delay_counter > 100:
+			queue_free()
+	
 func _on_Area2D_body_entered(body):
-	if body == Global.Player:
-		$".".queue_free()
+	if body == Global.Player and picked == false:
+		picked = true
+		$CollisionShape2D.disabled = true
+		$Sprite2.visible = false
+		$Sprite.visible = false
+		$Light2D.visible = false
+		$AudioStream_Pickup.play()	
+		#$".".queue_free()
 		Global.Player.add_orb( orb_color)
